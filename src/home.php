61cd0317905accea495383b4b2115e0a7a20ac30
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-17 20:58:00 +0100

    #10 Add some static menu cards in home page

--- src/home.php
+++ src/home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,9 +12,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body class="font-[Oswald]">
     <div class="min-h-[100vh] flex flex-col">
-        <nav class="flex items-center justify-between px-10 max-sm:px-2 shadow-md text-gray-700 bg-gray-50 z-10">
+        <nav class="flex items-center justify-between px-20 max-md:px-6 max-sm:px-2 shadow-md text-gray-700 sticky top-0 bg-gray-50 z-10">
             <ul class="h-full flex items-center">
                 <li class="mr-8" href="./index.html">
                     <a class="mr-8 flex items-center gap-1 cursor-pointer" href="./home.php">
@@ -23,35 +25,90 @@
             </ul>
             <div class="flex items-center flex-1 justify-between text-sm max-md:hidden">
                 <ul class="flex items-center">
-                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./dashboard.php">Home</a></li>
-                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./explore.php">Reservations</a></li>
-                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./explore.php">Menu</a></li>
-                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./explore.php">About</a></li>
-                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./explore.php">Contact</a></li>
+                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./home.php">Home</a></li>
+                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./reservation.php">Reservations</a></li>
+                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./menu.php">Menu</a></li>
+                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./about.php">About</a></li>
+                    <li><a class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear" href="./contact.php">Contact</a></li>
                 </ul>
                 <div class="flex items-center h-full gap-4">
-                    <a href="./pages/login.php" class="flex items-center hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear gap-2">
+                    <a href="./login.php" class="flex items-center hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear gap-2">
                         <span>Login</span>
-                        <img src="./assets/images/icons/user.svg" class="size-5" alt="">
+                        <img src="../assets/images/icons/user.svg" class="size-5" alt="">
                     </a>
                 </div>
             </div>
             <!-- <img src="../assets/images/icons/menu.svg" class="size-4 hidden max-md:block" alt=""> -->
         </nav>
 
-        <div class="flex-grow">
+        <div class="flex-grow flex flex-col gap-4">
             <div class="h-[80vh] relative">
                 <img src="https://d2d9hom4y5lr0f.cloudfront.net/2023/02/Kadeau-Copenhagen-Kitchen-Chefs-Marie-Louise-Munkegaard-Six-Michelin-Star-Restaurants-in-Copenhagen-Scandinavia-Standard.jpg" class="w-full object-cover h-full" alt="">
                 <div class="absolute top-0 bottom-0 w-full flex items-end justify-center">
                     <div class="gap-3 flex justify-center items-center pb-10 flex-col w-full bg-gradient-to-b from-transparent to-black">
-                        <p class="text-5xl text-white p-4">Welcome to Le Chef – Your Destination for Culinary Excellence!</p>
-                        <p class="text-white text-2xl w-2/3 max-lg:w-full px-4 text-center">
+                        <p class="text-5xl text-white p-4 max-md:text-2xl text-center">Welcome to Le Chef – Your Destination for Culinary Excellence!</p>
+                        <p class="text-white text-2xl w-2/3 max-lg:w-full px-4 text-center max-md:text-lg">
                             At Le Chef, we believe every meal tells a story. Our mission is to bring you a world of flavors, passion, and creativity straight to your table. From exquisite recipes to expert tips, we’re here to inspire your culinary journey and elevate every dining experience.
                         </p>
-                        <button class="bg-white p-2 px-4 rounded-full">Ready to awaken your inner chef? Let’s cook something extraordinary together!</button>
+                        <button class="bg-white p-2 px-4 rounded-full max-md:text-xs">Ready to awaken your inner chef? Let’s cook something extraordinary together!</button>
                     </div>
                 </div>
             </div>
+
+            <section class="flex flex-col gap-4 px-20 max-md:px-6 max-sm:px-2 pb-8">
+                <h1 class="text-2xl">Explore Menus</h1>
+
+                <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    <div class="grid gap-4">
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2F2019.2F09.2F20.2Fc7176c79-4cce-46da-a6db-786d43c41fbf.2Ejpeg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2F2019.2F09.2F20.2Fc7176c79-4cce-46da-a6db-786d43c41fbf.2Ejpeg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                    </div>
+                    <div class="grid gap-4">
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2Fvar.2Fcui.2Fstorage.2Fimages.2Fvideos.2Frecettes-en-video.2Ftourte-au-poulet-petits-legumes-et-orange-en-video.2F3791204-1-fre-FR.2Ftourte-au-poulet-petits-legumes-et-orange-en-video.2Ejpg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2Fvar.2Fcui.2Fstorage.2Fimages.2Fvideos.2Frecettes-en-video.2Ftourte-au-poulet-petits-legumes-et-orange-en-video.2F3791204-1-fre-FR.2Ftourte-au-poulet-petits-legumes-et-orange-en-video.2Ejpg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                    </div>
+                    <div class="grid gap-4">
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2F2019.2F09.2F20.2Fcb587bf6-f5d1-412f-a456-2a008646f51d.2Ejpeg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2F2019.2F09.2F20.2Fcb587bf6-f5d1-412f-a456-2a008646f51d.2Ejpeg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                    </div>
+                    <div class="grid gap-4">
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2F2019.2F10.2F07.2F618901eb-ec99-4008-99fe-c617cb098e74.2Ejpeg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                        <div class="flex flex-col gap-1">
+                            <img class="h-auto max-w-full rounded-lg" src="https://photo.cuisineactuelle.fr/imgre/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FCAC.2F2019.2F10.2F07.2F618901eb-ec99-4008-99fe-c617cb098e74.2Ejpeg/autox600/quality/65/crop-from/center/picture.jpeg" alt="">
+                            <p class="text-gray-700">Menu #234</p>
+                            <p class="text-gray-500 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptatem rem deleniti explicabo adipisci mollitia cupiditate minima. Iure, sed? Quaerat repellat itaque eaque temporibus odio.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
 
         <footer class="bg-gray-100 p-8 text-sm flex items-center justify-center">
@@ -59,4 +116,5 @@
         </footer>
     </div>
 </body>
+
 </html>
