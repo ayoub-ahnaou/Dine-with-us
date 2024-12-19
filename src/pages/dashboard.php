4253a8cbe88a88e7f39ed3096f855c276cf9b675
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-19 22:05:23 +0100

    #45 Include the navigation bar with php in all others pages (home, reservation, dashboard...)

--- src/pages/dashboard.php
+++ src/pages/dashboard.php
@@ -18,60 +18,7 @@
 
 <body class="font-[Oswald]">
   <div class="min-h-[100vh] flex flex-col">
-    <nav
-      class="flex items-center justify-between px-20 max-md:px-6 max-sm:px-2 shadow-md text-gray-700 sticky top-0 bg-gray-50 z-10">
-      <ul class="h-full flex items-center">
-        <li class="mr-8" href="./index.html">
-          <a
-            class="mr-8 flex items-center gap-1 cursor-pointer"
-            href="./home.php">
-            <span>Le-CHEF</span>
-          </a>
-        </li>
-      </ul>
-      <div
-        class="flex items-center flex-1 justify-between text-sm max-md:hidden">
-        <ul class="flex items-center">
-          <li>
-            <a
-              class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear"
-              href="./home.php">Home</a>
-          </li>
-          <li>
-            <a
-              class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear"
-              href="./reservation.php">Reservations</a>
-          </li>
-          <li>
-            <a
-              class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear"
-              href="./menu.php">Menu</a>
-          </li>
-          <li>
-            <a
-              class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear"
-              href="./about.php">About</a>
-          </li>
-          <li>
-            <a
-              class="hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear"
-              href="./contact.php">Contact</a>
-          </li>
-        </ul>
-        <div class="flex items-center h-full gap-4">
-          <a
-            href="./login.php"
-            class="flex items-center hover:bg-gray-100 hover:border-b-gray-700 border-transparent border p-2 px-4 cursor-pointer transition-all delay-75 ease-linear gap-2">
-            <span>Login</span>
-            <img
-              src="../assets/images/icons/user.svg"
-              class="size-5"
-              alt="" />
-          </a>
-        </div>
-      </div>
-      <!-- <img src="../assets/images/icons/menu.svg" class="size-4 hidden max-md:block" alt=""> -->
-    </nav>
+    <?php include "../assets/components/navigation.php"; ?>
 
     <div class="flex flex-grow w-full max-md:flex-col">
       <section class="w-1/5 bg-gray-100 flex flex-col max-md:w-full">
