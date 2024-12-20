d4ed1a980661039147ca93a465826dfb42f032d8
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-20 12:51:08 +0100

    #50 Prepare the table line with php syntaxe to integrate the users data.

--- src/pages/users-list.php
+++ src/pages/users-list.php
@@ -16,22 +16,24 @@
             </tr>
         </thead>
         <tbody>
-            <tr
-                class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <?php 
+            echo "<tr
+                class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
                 <th
-                    class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
                     1
                 </th>
-                <td class="px-6 py-2">Ayoub Ahnaou</td>
-                <td class="px-6 py-2">+212-654-506930</td>
-                <td class="px-6 py-2">email@gmail.com</td>
-                <td class="px-6 py-2">Client</td>
-                <td class="px-6 py-2 flex gap-2">
+                <td class='px-6 py-2'>Ayoub Ahnaou</td>
+                <td class='px-6 py-2'>+212-654-506930</td>
+                <td class='px-6 py-2'>email@gmail.com</td>
+                <td class='px-6 py-2'>Client</td>
+                <td class='px-6 py-2 flex gap-2'>
                     <a
-                        href="#"
-                        class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
+                        href='#'
+                        class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete</a>
                 </td>
-            </tr>
+            </tr>";
+            ?>
         </tbody>
     </table>
 </div>
