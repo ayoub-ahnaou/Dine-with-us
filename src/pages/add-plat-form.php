a1178efdfea6052c3f3e96a92c138badc9e935ca
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-20 21:39:24 +0100

    #71 Add form validation and error handling to add plat form using php

--- src/pages/add-plat-form.php
+++ src/pages/add-plat-form.php
@@ -17,6 +17,35 @@
     <div class="min-h-[100vh] flex flex-col">
         <?php include "../assets/components/navigation.php"; ?>
 
+        <?php
+        $title = $type = $ingradiants = $picture = "";
+        $query_err = $title_err = $type_err = $ingradiants_err = $picture_err = "";
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $title = $_POST["plat_title"];
+            $type = $_POST["plat_type"];
+            $ingradiants = $_POST["ingrediants"];
+            $picture = $_POST["plat_picture"];
+
+            if(empty($title)) $title_err = "Plat title is required";
+            if(empty($type)) $type_err = "Plat type is required";
+            if(empty($ingradiants)) $ingradiants_err = "Plat ingradiants is required";
+            if(empty($picture)) $picture_err = "Plat picture is required";
+
+            if(!empty($title) && !empty($type) && !empty($ingradiants) && !empty($picture)){
+                $insert_plat_statment = $connect->prepare("INSERT INTO plat (plat_title, ingredients, plat_type, plat_picture) VALUES (?, ?, ?, ?)");
+                $insert_plat_statment->bind_param("ssss", $title, $ingradiants, $type, $picture);
+
+                if($insert_plat_statment->execute()){
+                    header("location: dashboard.php");
+                }
+                else {
+                    $error_query = "Something Went Wrong While Inserting, Try Again Later";
+                }
+            }
+        }
+        ?>
+
         <div class="bg-black h-6"></div>
         <div class="flex justify-center items-center flex-grow flex-col gap-4 px-20 max-md:px-6 max-sm:px-2 py-4 bg-white">
             <form method="post" class="max-w-lg mx-auto bg-white shadow-md h-auto p-2 rounded-md w-1/2 text-sm">
@@ -25,24 +54,24 @@
                     <div class="flex flex-col relative">
                         <label name="query_err" class="text-red-500 bg-red-50"></label>
                         <div class="flex flex-col">
-                            <label for="plat-title" class="text-gray-500">Plat Title</label>
-                            <input type="text" name="plat-title" id="plat-title" placeholder="e.g: Entrée, Principale, Dessert..." class="bg-gray-100 rounded-sm p-1">
-                            <label name="title_err" class="text-red-500"></label>
+                            <label for="plat_title" class="text-gray-500">Plat Title</label>
+                            <input value="<?php echo $title; ?>" type="text" name="plat_title" id="plat_title" placeholder="e.g: Entrée, Principale, Dessert..." class="bg-gray-100 rounded-sm p-1">
+                            <label name="title_err" class="text-red-500"><?php echo $title_err; ?></label>
                         </div>
                         <div class="flex flex-col">
                             <label for="plat_type" class="text-gray-500">Plat Type</label>
-                            <input type="text" name="plat_type" id="plat-_type" placeholder="e.g: Entrée, Principale, Dessert..." class="bg-gray-100 rounded-sm p-1">
-                            <label name="plat_type_err" class="text-red-500"></label>
+                            <input value="<?php echo $type ; ?>" type="text" name="plat_type" id="plat-_type" placeholder="e.g: Entrée, Principale, Dessert..." class="bg-gray-100 rounded-sm p-1">
+                            <label name="plat_type_err" class="text-red-500"><?php echo $type_err; ?></label>
                         </div>
                         <div class="flex flex-col">
                             <label for="ingrediants" class="text-gray-500">Ingrediants</label>
-                            <input type="text" name="ingrediants" id="ingrediants" placeholder="Enter the ingradiants of your plat" class="bg-gray-100 rounded-sm p-1">
-                            <label name="ingradiants_err" class="text-red-500"></label>
+                            <input value="<?php echo $ingradiants; ?>" type="text" name="ingrediants" id="ingrediants" placeholder="Enter the ingradiants of your plat" class="bg-gray-100 rounded-sm p-1">
+                            <label name="ingradiants_err" class="text-red-500"><?php echo $ingradiants_err; ?></label>
                         </div>
                         <div class="flex flex-col">
                             <label class="text-gray-500" for="plat_picture">Plat Picture</label>
-                            <input class="text-gray-900 cursor-pointer bg-gray-100" id="plat_picture" type="file">
-                            <label name="picture_err" class="text-red-500"></label>
+                            <input value="<?php echo $picture; ?>" class="text-gray-900 cursor-pointer bg-gray-100" name="plat_picture" id="plat_picture" type="file">
+                            <label name="picture_err" class="text-red-500"><?php echo $picture_err; ?></label>
                         </div>
                     </div>
                 </div>
