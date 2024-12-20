409c63634dbb71630ac592ad6664cb470d3c68be
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-20 13:19:55 +0100

    #50 Create the query to fetch users data and place it in the table

--- src/pages/users-list.php
+++ src/pages/users-list.php
@@ -1,3 +1,9 @@
+<?php
+$all_users = $connect->prepare("SELECT user_id, username, phone, email, user.role_id, role_name FROM user, role WHERE user.role_id = role.role_id");
+$all_users->execute();
+$all_users->bind_result($user_id, $username, $phone, $email, $role_id, $role_name);
+?>
+
 <div id="users-list" class="hidden w-4/5 overflow-auto flex-grow max-md:w-full">
     <div class="flex p-6">
         <p class="text-xl">Users</p>
@@ -16,23 +22,25 @@
             </tr>
         </thead>
         <tbody>
-            <?php 
-            echo "<tr
-                class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
-                <th
-                    class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
-                    1
-                </th>
-                <td class='px-6 py-2'>Ayoub Ahnaou</td>
-                <td class='px-6 py-2'>+212-654-506930</td>
-                <td class='px-6 py-2'>email@gmail.com</td>
-                <td class='px-6 py-2'>Client</td>
-                <td class='px-6 py-2 flex gap-2'>
-                    <a
-                        href='#'
-                        class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete</a>
-                </td>
-            </tr>";
+            <?php
+            while ($all_users->fetch()) {
+                echo "<tr
+                    class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
+                    <th
+                        class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                        $user_id
+                    </th>
+                    <td class='px-6 py-2'>$username</td>
+                    <td class='px-6 py-2'>$phone</td>
+                    <td class='px-6 py-2'>$email</td>
+                    <td class='px-6 py-2'>$role_name</td>
+                    <td class='px-6 py-2 flex gap-2'>
+                        <a
+                            href='#'
+                            class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete</a>
+                    </td>
+                </tr>";
+            }
             ?>
         </tbody>
     </table>
