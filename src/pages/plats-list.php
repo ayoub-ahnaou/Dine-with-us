96951072dde81176f7f2dfcbed55be46f4792039
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-20 23:58:21 +0100

    #52 Get the plats data and make it in its place in table

--- src/pages/plats-list.php
+++ src/pages/plats-list.php
@@ -1,3 +1,9 @@
+<?php 
+$all_plats_statment = $connect->prepare("SELECT plat_id, plat_title, ingredients, plat_type, plat_picture FROM plat");
+$all_plats_statment->execute();
+$all_plats_statment->bind_result($plat_id, $plat_title, $ingredients, $plat_type, $plat_picture);
+?>
+
 <div id="plats-list" class="hidden w-4/5 overflow-auto flex-grow max-md:w-full">
     <div class="flex p-6 justify-between pr-20">
         <p class="text-xl">Plats</p>
@@ -21,27 +27,32 @@
                 </tr>
             </thead>
             <tbody>
-                <tr
-                    class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                    <th
-                        class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        1
-                    </th>
-                    <td class="px-6 py-2">Chocholat Fromagere</td>
-                    <td class="px-6 py-2">Dissert</td>
-                    <td class="px-6 py-2">Lorem ipsum dolor sit amet consectetur adipisicing eli...</td>
-                    <td class="px-6 py-2">45.60$</td>
-                    <td class="px-6 py-2 flex gap-2">
-                        <a
-                            href="#"
-                            class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete
-                        </a>
-                        <a
-                            href="#"
-                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Update
-                        </a>
-                    </td>
-                </tr>
+                <?php 
+                    while($all_plats_statment->fetch()){
+                        echo "<tr
+                            class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
+                            <th
+                                class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                                $plat_id
+                            </th>
+                            <td class='px-6 py-2'>$plat_title</td>
+                            <td class='px-6 py-2'>$plat_type</td>
+                            <td class='px-6 py-2'>$ingredients</td>
+                            <td class='px-6 py-2'>99.99$</td>
+                            <td class='px-6 py-2 flex gap-2'>
+                                <a
+                                    href='#'
+                                    class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete
+                                </a>
+                                <a
+                                    href='#'
+                                    class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Update
+                                </a>
+                            </td>
+                        </tr>";
+                    }
+                    // echo "<p class='text-red-500 bg-red-50 p-1'>No plats is available</p>";
+                ?>
             </tbody>
         </table>
     </div>
