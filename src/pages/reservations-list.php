7c43e98a2dd7908f815596bfa484405c1889232c
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-21 15:51:03 +0100

    #51 Close prepared statements after execution in menu, plats, and users lists and place the reservations list in table

--- src/pages/reservations-list.php
+++ src/pages/reservations-list.php
@@ -1,3 +1,12 @@
+<?php 
+$all_reservations = $connect->prepare("SELECT r.reservation_id, r.user_id, user.username, reservation_date, user.phone, user.email
+FROM reservation_menu AS rm, menu AS m, reservation AS r, user
+WHERE rm.reservation_id = r.reservation_id AND rm.menu_id = m.menu_id AND user.user_id = r.user_id");
+
+$all_reservations->execute();
+$all_reservations->bind_result($reservation_id, $user_id, $username, $reservation_date, $phone, $email);
+?>
+
 <div id="reservations-list" class="hidden w-4/5 overflow-auto flex-grow max-md:w-full">
     <div class="flex p-6">
         <p class="text-xl">Reservations</p>
@@ -17,28 +26,32 @@
             </tr>
         </thead>
         <tbody>
-            <tr
-                class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <?php 
+            while($all_reservations->fetch()){
+                echo "<tr
+                class='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
                 <th
-                    class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    1
+                    class='px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+                    $reservation_id
                 </th>
-                <td class="px-6 py-2">Ayoub Ahnaou</td>
-                <td class="px-6 py-2">12/12/2024 12:23 AM</td>
-                <td class="px-6 py-2">+212-654-506930</td>
-                <td class="px-6 py-2">email@gmail.com</td>
-                <td class="px-6 py-2">Pendding</td>
-                <td class="px-6 py-2 flex gap-2">
+                <td class='px-6 py-2'>$username</td>
+                <td class='px-6 py-2'>$reservation_date</td>
+                <td class='px-6 py-2'>$phone</td>
+                <td class='px-6 py-2'>$email</td>
+                <td class='px-6 py-2'>Pendding</td>
+                <td class='px-6 py-2 flex gap-2'>
                     <a
-                        href="#"
-                        class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete
+                        href='#'
+                        class='font-medium text-red-600 dark:text-red-500 hover:underline'>Delete
                     </a>
                     <a
-                        href="#"
-                        class="font-medium text-green-600 dark:text-green-500 hover:underline">Marke as Done
+                        href='#'
+                        class='font-medium text-green-600 dark:text-green-500 hover:underline'>Marke as Done
                     </a>
                 </td>
-            </tr>
+            </tr>";
+            }
+            ?>
         </tbody>
     </table>
 </div>
